446432ffea1ca9d94b516c24e7ac72e35fe8b6b1
ortiztorresdanielenrique1019 <ortiztorresdanielenrique@gmail.com>
2026-05-02 10:01:30 -0400

    subir archivos

---
+++ ejemolos.py
@@ -0,0 +1,26 @@
+# ejemolos de python para formac;cion complemetaria
+x = 5
+y = 10
+
+print ("el valor de 5 * 10 es",x  *y)
+print ("realizado")
+
+nombre = input("el nombre completo es:"+ nombre +"" + apellido )
+apellido = input("coloca apellido")
+
+print ("el nombre completo es",nombre + " " + apellido)
+
+edad = input("coloca tu edad")
+
+total = int(edad) + 1500
+print (total)
+
+if ( edad < 18):
+    print ("eres menor de edad")
+    else:
+    print ("eres mayor de edad")
+    
+    def saludar(nombre):
+        print ("hola",nombre)
+
+        """............"""
